2a1a634a7313365c71c5b954e505bdd29a5d1f88
Inheritance Kills <20968832+RickOrchard@users.noreply.github.com>
2020-08-04 16:42:58 +0200

    Update and rename jpg-convert.sh to convert-jpg.sh

--- convert/convert-jpg.sh
+++ convert/convert-jpg.sh
@@ -12,9 +12,9 @@ echo ""
 echo -e -n "  Converting \e[1m $f:\e[0m  "
 #printf "  q5-huge..." && convert "$f" -quality 85% -strip -filter Lanczos -colorspace RGB -resize "3840x3840>" -unsharp 0x0.75+0.75+0.008 -colorspace sRGB "./webconvert-jpg/q5-${f}"
 printf "  q4-large..." && convert "$f" -quality 85% -strip -filter Lanczos -colorspace RGB -resize "1920x1920>" -unsharp 0x0.75+0.75+0.008 -colorspace sRGB "./webconvert-jpg/q4-${f}"
-#printf "  q3-medium..." && convert "$f" -quality 90% -strip -filter Lanczos -colorspace RGB -resize "720x720>" -unsharp 0x0.75+0.75+0.008 -colorspace sRGB "./webconvert-jpg/q3-${f}"
-#printf "  q2-small..." && convert "$f" -quality 95% -strip -filter Lanczos -colorspace RGB -resize "240x240>" -unsharp 0x0.75+0.75+0.008 -colorspace sRGB "./webconvert-jpg/q2-${f}"
-#printf "  q1-tiny..." && convert "$f" -quality 95% -strip -filter Lanczos -colorspace RGB -resize "90x90>" -unsharp 0x0.75+0.75+0.008 -colorspace sRGB "./webconvert-jpg/q1-${f}"
+printf "  q3-medium..." && convert "$f" -quality 90% -strip -filter Lanczos -colorspace RGB -resize "720x720>" -unsharp 0x0.75+0.75+0.008 -colorspace sRGB "./webconvert-jpg/q3-${f}"
+printf "  q2-small..." && convert "$f" -quality 95% -strip -filter Lanczos -colorspace RGB -resize "240x240>" -unsharp 0x0.75+0.75+0.008 -colorspace sRGB "./webconvert-jpg/q2-${f}"
+printf "  q1-tiny..." && convert "$f" -quality 95% -strip -filter Lanczos -colorspace RGB -resize "90x90>" -unsharp 0x0.75+0.75+0.008 -colorspace sRGB "./webconvert-jpg/q1-${f}"
 
 if [[ $1 == "--rename" ]]
   then
