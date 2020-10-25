cc793becc40a00ed8dcbb0ea3d1415a312bc76ed
Inheritance Kills <20968832+RickOrchard@users.noreply.github.com>
2020-10-25 16:41:14 +0100

    Update upscale_png.sh

--- convert/blend/upscale_png.sh
+++ convert/blend/upscale_png.sh
@@ -12,33 +12,35 @@ mkdir -p ./blended
 for f in *.png
 do
 
-# IMAGEMAGICK, convert point generates the most detail but is better used with exposure blends because of pixelated appearance
-echo -e "\n"
-echo -e "\e[0m  Upscaling using convert... \e[1m $f: \e[2m"
-time convert "$f" -filter point -colorspace RGB -resize "300%" -colorspace sRGB "./upscaled/${f}.up_convert_point.png"
-#time convert "$f" -filter point -colorspace RGB -resize "300%" -quality "100%" "./upscaled/${f}.up_convert_point.jpg"
+## Select one of the 2 upscaling methods below, or both for even better detail
 
-
-# XBRZSCALE, best for non-stacked images
+## 1. IMAGEMAGICK, convert point generates the most detail but is better used with exposure blends because of pixelated appearance
 #echo -e "\n"
-#echo -e "\e[0m  Upscaling using xbrzscale... \e[1m $f: \e[2m"
-#time xbrzscale 3 "$f" "./upscaled/${f}.up_xbrzscale.png"
+echo -e "\e[0m »»» Upscaling using convert: \e[1m $f... \e[2m"
+time convert "$f" -filter point -resize "300%" "PNG24:./upscaled/${f}.up_convert_point.png"
 
 
-if [[ $1 == "--rename" ]]
-  then
-    mv "$f" "qbase-${f}"
-fi
+## 2. XBRZSCALE, best for non-stacked images
+echo -e "\n"
+echo -e "\e[0m »»» Upscaling using xbrzscale: \e[1m $f... \e[2m"
+time xbrzscale 3 "$f" "./upscaled/${f}.up_xbrzscale.png"
+
+
+#if [[ $1 == "--rename" ]]
+#  then
+#    mv "$f" "qbase-${f}"
+#fi
 
 done
 
 
 echo "\n"
-echo -e "\e[0m  Blending multiple exposures using enfuse... \e[2m"
-time enfuse --output="./blended/blend_enfuse-${f}.jpg" --compression=100 ./upscaled/*.png
+echo -e "\e[0m »»» Blending multiple exposures using enfuse... \e[2m"
+time enfuse --output="./blended/blend_enfuse-${f}.jpg" --compression=100 ./upscaled/*.png --verbose=0
+#time enfuse --output="./blended/blend_enfuse-${f}.png" ./upscaled/*.png
 
 echo "\n"
-echo -e "\e[0m  Copying EXIF data to the new file... \e[2m"
+echo -e "\e[0m »»» Copying EXIF data to the new file... \e[2m"
 exiv2 rm "./blended/blend_enfuse-${f}.jpg"
 exiv2 -ea- "$f" | exiv2 -ia- "./blended/blend_enfuse-${f}.jpg"
 #rm -Rf "./upscaled/*"
