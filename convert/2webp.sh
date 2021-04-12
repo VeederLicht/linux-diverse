ae608791b66d09b31dc1bc84efc02637d50020f3
Inheritance Kills <20968832+RickOrchard@users.noreply.github.com>
2021-04-12 11:38:33 +0200

    Create 2webp.sh

---
+++ convert/2webp.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+clear
+
+
+# Define text styles
+sYe="\e[93m"
+sNo="\e[36m"
+
+
+# Show banner
+echo -e "\n ${sYe}"
+echo -e "  ============================================================================"
+echo -e "  An image2WEBP conversion script using ffmpeg, RickOrchard 2021, no copyright"
+echo -e "  ------------------------------ v1.0 ----------------------------------------"
+echo -e "${sNo}"
+
+
+# Set arguments
+    i="jpg"
+    r=99999
+    q=85
+    x="-map_metadata -1"  # by default, DONT copy metadata/exif
+    
+    while getopts ":i:r:q:x:" opt; do
+      case $opt in
+        #i) i="${OPTARG:-jpg}"   # If i not set or null, use default
+        i) i=$OPTARG
+           echo "    file-type=${i}"
+        ;;
+        r) r=$OPTARG
+           echo "    resolution=${r}"
+        ;;
+        q) q=$OPTARG
+           echo "    quality=${q}"
+        ;;
+        x) x="-map_metadata 0"
+           echo "    keep-metadata=yes"
+        ;;
+        \?) echo "Invalid option -$OPTARG" >&2
+        ;;
+      esac
+    done
+
+
+# Execute
+	echo "WEBP batch conversion script - output"  > 2webp.rep
+	date  >> 2webp.rep
+	echo -e "+++++++++++++++++++++++++++++++++++++++++++\n\n\n" >> 2webp.rep
+	
+    echo -e "\n"
+
+	for f in *.$i
+	do
+
+		echo "    Processing ${f}..."
+		echo -e "\n\n\n  ⟹  PROCESSING  ${f}:" >> 2webp.rep
+
+		## De-interlace
+		ffmpeg -loglevel repeat+level+verbose -i $f -vf "pp7,bm3d=sigma=2,scale=w='min(iw,$r)':h='min(ih,$r)':force_original_aspect_ratio=decrease,unsharp=3:3:1" $x -q:v $q $f.webp -y 2>> 2webp.rep
+
+	done
+
+
+	echo -e "\n\n\n+++++++++++++++++++++++++++++++++++++++++++" >> 2webp.rep
+	echo "   BATCH FINISHED"  >> 2webp.rep
+	date  >> 2webp.rep
+
+
+echo -e "\n\n\n  ${sYe} Finished \e[5m              ´jpeg is outdated, please stop using it...´ \e[0m \n\n\n"
