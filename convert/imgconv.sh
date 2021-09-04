8ab8022c73c523d0a03fd5f9b8222f524042ce80
Inheritance Kills <20968832+RickOrchard@users.noreply.github.com>
2021-09-04 17:32:41 +0200

    Some code cleanup

--- convert/imgconv.sh
+++ convert/imgconv.sh
@@ -10,7 +10,7 @@ m_comment=''
 clear
 
 # Define constants
-scriptv="v0.93"
+scriptv="v0.93b"
 sYe="\e[93m"
 sNo="\033[1;35m"
 logfile=$(date +%Y%m%d_%H.%M_)"imgconv.rep"
@@ -44,30 +44,29 @@ echo -e "  -------------------------------------imgconv.sh $scriptv logfile-----
 
 
 	# ... select output format
-	read -e "      Select output format: "
 	echo -e "\n"
+	echo -e "      SELECT OUTPUT FORMAT: "
 	echo -e "     (a) Convert to avif (av1, no support for metadata)"
 	echo -e "     (w) Convert to webp"
 	echo -e "     (j) Convert to jpg"
 	echo -e ""
-	read -p "      --> " answer1
+	read -p "      --> " answer_format
 	echo -e ""
 	
 
 	# ... select quality
-	read -e "      Select quality: "
-	echo -e "\n"
+	echo -e "      SELECT QUALITY: "
 	echo -e "     (l) low quality"
 	echo -e "     (m) medium quality"
 	echo -e "     (h) high quality"
 	echo -e ""
-	read -p "      --> " answer1
+	read -p "      --> " answer_quality
 	echo -e ""
 
-	case $answer1 in
+	case $answer_format in
 	  "a")
         arg0="avif"
-        case $quality in
+        case $answer_quality in
           "l")
                 arg9="-quality 45"
             ;;
@@ -83,7 +82,7 @@ echo -e "  -------------------------------------imgconv.sh $scriptv logfile-----
 		;;
 	  "w")
         arg0="webp"
-        case $quality in
+        case $answer_quality in
           "l")
                 arg9="-quality 50"
             ;;
@@ -99,7 +98,7 @@ echo -e "  -------------------------------------imgconv.sh $scriptv logfile-----
 		;;
 	  "j")
         arg0="jpg"
-        case $quality in
+        case $answer_quality in
           "l")
                 arg9="-quality 55"
             ;;
@@ -118,13 +117,14 @@ echo -e "  -------------------------------------imgconv.sh $scriptv logfile-----
 		exit 3
 		;;
 	esac
-    echo -e "  -----------------Convert to $arg0 with $arg9 \n" >> $logfile
+    echo -e "  -----------------Convert to [ $arg0 ] with [ $arg9 ] \n" >> $logfile
 
 
 
 
 	# ... select noise reduction
 	echo -e "\n"
+	echo -e "      SELECT DENOISING LEVEL: "
 	echo -e "     (n) none"
 	echo -e "     (l) light"
 	echo -e "     (m) medium"
@@ -132,32 +132,27 @@ echo -e "  -------------------------------------imgconv.sh $scriptv logfile-----
 	echo -e "     (v) very strong"
 	echo -e "     (c) cartoonize"
 	echo -e ""
-	read -p "      Select denoising level: " answer1
+	read -p "      --> " answer_noise
 	echo -e ""
 
-	case $answer1 in
+	case $answer_noise in
 	  "n")
         echo -e "  -----------------No noise reduction \n" >> $logfile
         arg1=""
 		;;
 	  "l")
-        echo -e "  -----------------Light noise reduction \n" >> $logfile
         arg1="-bilateral-blur 3"
 		;;
 	  "m")
-        echo -e "  -----------------Medium noise reduction \n" >> $logfile
         arg1="-wavelet-denoise 1%"
 		;;
 	  "s")
-        echo -e "  -----------------Strong noise reduction \n" >> $logfile
         arg1="-enhance"
 		;;
 	  "v")
-        echo -e "  -----------------Very strong noise reduction \n" >> $logfile
         arg1="-despeckle"
 		;;
 	  "c")
-        echo -e "  -----------------Cartoonize noise reduction \n" >> $logfile
         arg1="-kuwahara 4"
 		;;
 	  *)
@@ -165,36 +160,32 @@ echo -e "  -------------------------------------imgconv.sh $scriptv logfile-----
 		exit 3
 		;;
 	esac
-
+    echo -e "  ----------------[ $arg1 ]  \n" >> $logfile
 
 
 
 
 	# ... select sharpening
-	echo -e "\n"
+	echo -e "      SELECT SHARPENING LEVEL: "
 	echo -e "     (n) none"
 	echo -e "     (l) light"
 	echo -e "     (m) medium"
 	echo -e "     (s) strong"
 	echo -e ""
-	read -p "      Select sharpening level: " answer1
+	read -p "      --> " answer_sharpen
 	echo -e ""
 
-	case $answer1 in
+	case $answer_sharpen in
 	  "n")
-        echo -e "  -----------------No sharpening \n" >> $logfile
         arg2=""
 		;;
 	  "l")
-        echo -e "  -----------------Light sharpening \n" >> $logfile
         arg2="-adaptive-sharpen 1"
 		;;
 	  "m")
-        echo -e "  -----------------Medium sharpening \n" >> $logfile
         arg2="-adaptive-sharpen 2"
 		;;
 	  "s")
-        echo -e "  -----------------Strong sharpening \n" >> $logfile
         arg2="-unsharp 1"
 		;;
 	  *)
@@ -202,23 +193,24 @@ echo -e "  -------------------------------------imgconv.sh $scriptv logfile-----
 		exit 3
 		;;
 	esac
+    echo -e "  ----------------[ $arg2 ]  \n" >> $logfile
 
 
 
 
 
 	# ... select resize
-	echo -e "\n"
+	echo -e "      SELECT RESIZING: "
 	echo -e "     (n) no, keep original"
 	echo -e "     (s) small, max 640p"
 	echo -e "     (h) HD, max 1280p"
 	echo -e "     (2) 2K, max 2560p"
 	echo -e "     (4) 4K, max 3840p"
 	echo -e ""
-	read -p "      Resize image: " answer1
+	read -p "      --> " answer_size
 	echo -e ""
 
-	case $answer1 in
+	case $answer_size in
 	  "n")
         arg3=""
 		;;
@@ -239,22 +231,22 @@ echo -e "  -------------------------------------imgconv.sh $scriptv logfile-----
 		exit 3
 		;;
 	esac
-    echo -e "  ----------------$arg3  \n" >> $logfile
+    echo -e "  ----------------[ $arg3 ]  \n" >> $logfile
 
 
 
 
 
-	# ... select resize
-	echo -e "\n"
+	# ... select contrast
+	echo -e "      SELECT CONTRAST NORMALIZATION"
 	echo -e "     (n) none, keep original"
 	echo -e "     (N) natural"
 	echo -e "     (M) mathematic"
 	echo -e ""
-	read -p "      Select contrast normalization: " answer1
+	read -p "      --> " answer_contrast
 	echo -e ""
 
-	case $answer1 in
+	case $answer_contrast in
 	  "n")
         arg4=""
 		;;
@@ -269,7 +261,7 @@ echo -e "  -------------------------------------imgconv.sh $scriptv logfile-----
 		exit 3
 		;;
 	esac
-    echo -e "  ----------------$arg4  \n" >> $logfile
+    echo -e "  ----------------[ $arg4 ] \n" >> $logfile
 
 
 
