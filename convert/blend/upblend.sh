191108b5ac5edb13d15cc2b793b40ad10b456796
InheritanceKills <20968832+RickOrchard@users.noreply.github.com>
2023-03-15 16:13:56 +0100

    Major changes
    Select stack size
    - enfuse
    - align_image_stack
    - exiv2 copy EXIF
    - waifu2x upscale

--- convert/blend/upblend.sh
+++ convert/blend/upblend.sh
@@ -10,7 +10,7 @@ m_year=''
 clear
 
 # Define constants
-scriptv="v1.31"
+scriptv="v2.0"
 sYe="\e[93m"
 sNo="\033[1;35m"
 logfile=$(date +%Y%m%d_%H.%M_)"upblend.rep"
@@ -18,79 +18,106 @@ logfile=$(date +%Y%m%d_%H.%M_)"upblend.rep"
 # Show banner
 echo -e "\n ${sNo}"
 echo -e "  ======================================================================================================="
-echo -e "              An image batch-upscale-&-merge script, copyleft 2020 RickOrchard@Github"
+echo -e "              An image batch-upscale-&-merge script, copyleft 2023 RickOrchard@Github"
 echo -e "  --------------------------------------------${sYe} $scriptv ${sNo}----------------------------------------------------"
 echo -e "\n ${sYe}  NOTE: metadata will be injected, to change it edit this scriptheader!  ${sNo} \n\n"
 
 # Test nr. of arguments
+exitcode=0
 if [ $# -eq 0 ]
-  then
-    echo "        No source files specified."
-	exit 2
+then
+	echo "        No source files specified."
+	exit
+fi
+if ! command -v enfuse &> /dev/null
+then
+	echo "Essential program is not installed: enfuse"
+	exit
+fi
+if ! command -v align_image_stack &> /dev/null
+then
+	echo "Essential program is not installed: align_image_stack"
+	exit
+fi
+if ! command -v waifu2x-ncnn-vulkan &> /dev/null
+then
+	echo "Essential program is not installed: waifu2x-ncnn-vulkan"
+	exit
+fi
+if ! command -v exiv2 &> /dev/null
+then
+	echo "Essential program is not installed: exiv2"
+	exit
 fi
 
+
 iStart=$(date +%s)
+usdir=./upblend.d
+tmpdir=$usdir/0.tmp
+blenddir=$usdir/1.blended
+scaledir=$usdir/2.scaled
 
-mkdir -p ./upscaled
-mkdir -p ./blended
 
+echo -e ""
+read -p "      Enter the size of each image stack:" answer_size
+echo -e ""
+if ! [[ "$answer_size" =~ ^[0-9]+$ ]]
+	then
+		echo -e "    ERROR:  Enter whole numbers only, exiting...."
+		exit
+fi
 
-# !!!! ARGUMENTS IN DOUBLE QOUTES TO AVOID PROBLEMS WITH SPACES IN FILENAMES!!! https://stackoverflow.com/questions/12314451/accessing-bash-command-line-args-vs
+rm -Rf $usdir
+mkdir -p $tmpdir
+mkdir -p $blenddir
+mkdir -p $scaledir
+
+echo -e "\n \e[0m      »»» Blending multiple exposures using enfuse... \e[2m \n"
+
+count=0
 for f in "$@"
-# for f in *.png
 do
-
-	## Uncomment one or more of the upscaling methods below, keep in mind that the memory requirements rise incrementally
-
-	## 1. IMAGEMAGICK, convert point generates the most detail but is better used with many exposure blends because of pixelated appearance
-#		echo -e "\n"
-#		echo -e -n "\e[0m »»» Upscaling using convert: \e[1m $f... \e[2m"
-#		time convert "$f" -filter point -resize "200%" "PNG24:./upscaled/${f}.up_convert_point.png"
-#		time convert "$f" -sigmoidal-contrast 1x20% -sigmoidal-contrast 1x55% -modulate 100,85 -normalize -filter point -resize "200%" "./upscaled/${f}.up_convert_point.jpg"
-
-
-	## 2. XBRZSCALE, upscaling using antialiasing
-#		echo -e "\n"
-#		echo -e "\e[0m »»» Upscaling using xbrzscale: \e[1m $f... \e[2m"
-#		time xbrzscale 2 "$f" "./upscaled/${f}.up_xbrzscale.png"
-
-
-	## 3. WAIFU2X, best overall but slowest (GPU required)
-		echo -e "\n"
-		echo -e "\e[0m »»» Upscaling using waifu2x: \e[1m $f... \e[2m"
-		time waifu2x-ncnn-vulkan -i "$f" -s 2 -f jpg -o "./upscaled/${f}.up_waifu2x.jpg"
-
-	#if [[ $1 == "--rename" ]]
-	#  then
-	#    mv "$f" "qbase-${f}"
-	#fi
+	((count++))
+echo -e "-------$f"
+	cp "$f" $tmpdir/
+	if [ $count -eq $answer_size ]
+	then
+		align_image_stack --gpu -a "${tmpdir}/"aligned -C "${tmpdir}/"*
+		outfile="${blenddir}/${f}#enfused.jpg"
+		enfuse --compression=90 --verbose=0 --exposure-weight-function=gaussian --output="${outfile}" "${tmpdir}/"*.tif
+		exiv2 rm "${outfile}"
+		exiv2 -ea- "$f" | exiv2 -ia- "${outfile}"
+		if [ $? -eq 0 ]
+		then
+			exiv2 -r':basename:_exif' "${outfile}"
+		fi
+		rm -f "${tmpdir}/"*
+		count=0
+	fi
 
 done
 
 
-## ALIGN_IMAGE_STACK, doesnt work for 360 photos
-	#echo "\n"
-	#echo -e "\e[0m »»» Aligning images using align_image_stack... \e[2m"
-	#align_image_stack --gpu -a alligned.tiff -C *.png
 
-
-## ENFUSE, blend all exposures together
-	echo -e "\n"
-	echo -e "\e[0m »»» Blending multiple exposures using enfuse... \e[2m"
-	time enfuse --output="./blended/${f}#enfused.jpg" --compression=90 ./upscaled/*.jpg --verbose=0 --exposure-optimum=0.4 --exposure-width=0.1 --exposure-weight-function=gaussian
-
-## EXIV2, copy EXIF data to new file
-	echo -e "\n"
-	echo -e "\e[0m »»» Copying EXIF data to the new file... \e[2m"
-	exiv2 rm "./blended/${f}#enfused.jpg"
-	exiv2 -ea- "$f" | exiv2 -ia- "./blended/${f}#enfused.jpg"
+# UPSCALE, nog aanpassen naar brondirectory, evt EXIF kopieren
+for f in "${blenddir}/"*
+do
+	infile=$(basename -- "${f}")
+	outfile="${scaledir}/${infile}#w2x.jpg"
+	echo -e "\e[0m »»» Upscaling using waifu2x: \e[1m $infile... \e[2m"
+	waifu2x-ncnn-vulkan -i "$f" -s 2 -f jpg -o "${outfile}"
+	exiv2 rm "${outfile}"
+	exiv2 -ea- "$f" | exiv2 -ia- "${outfile}"
 	if [ $? -eq 0 ]
 	then
-		exiv2 -r':basename:_exif' "./blended/${f}#enfused.jpg"
+		exiv2 -r':basename:_exif' "${outfile}"
 	fi
-	
-#rm -Rf "./upscaled/*"
+done
+
 
 iEnd=$(date +%s)
 echo -e "\n \e[0m »»» Total processing time: $((iEnd-iStart)) seconds \e[2m"
 echo -e "\n\n \e[4mFinished!\e[0m \n\n\n"
+
+# !!!! ARGUMENTS IN DOUBLE QOUTES TO AVOID PROBLEMS WITH SPACES IN FILENAMES!!! https://stackoverflow.com/questions/12314451/accessing-bash-command-line-args-vs
+# !!!! In order to make use of the wildcard I need to leave * outside of the quotes  https://stackoverflow.com/questions/50354995/bash-wildcard-working-in-command-line-but-not-script
